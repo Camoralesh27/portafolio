4c031e812023c8cbfa78dbac07693336d5ebcad5
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-07-14 23:39:26 -0600

    realizando dark-theme pero aun no funciona el cambio de variables scss

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ const navToggle = document.querySelector('#nav-toggle');
 const navClose = document.querySelector('#nav-close');
 
 
+
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle) {
@@ -13,6 +14,7 @@ if(navToggle) {
 }
 
 
+
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if(navClose) {
@@ -20,6 +22,7 @@ if(navClose) {
         navMenu.classList.remove('show-menu');
     })
 }
+
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -32,6 +35,7 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -55,6 +59,7 @@ skillsHeader.forEach((el) => {
 })
 
 
+
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]');
 const tabContents =document.querySelectorAll('[data-content]');
@@ -75,6 +80,8 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active');
     })
 })
+
+
 
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal');
@@ -99,6 +106,8 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
+
+
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
@@ -112,6 +121,8 @@ let swiperPortfolio = new Swiper(".portfolio__container", {
       clickable: true,
     },
 });
+
+
 
 /*==================== TESTIMONIAL SWIPER ====================*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
@@ -155,6 +166,8 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
     const nav = document.getElementById('header')
@@ -162,6 +175,8 @@ function scrollHeader(){
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
 
 /*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
@@ -171,4 +186,34 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp)
 
+
+
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
