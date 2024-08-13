e94762c340398f01c892feb1b19136be83ae042d
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-08-13 08:46:59 -0600

    terminando json language, siguiente modificar seccion skills

--- src/js/app.js
+++ src/js/app.js
@@ -369,20 +369,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-// Cambiar Idioma
+/*==================== CAMBIAR IDIOMA ====================*/
 const flagsElement = document.querySelector('#flags');
 const textsToChange = document.querySelectorAll('[data-section]');
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email');
+const inputMessage = document.querySelector('#message')
 
 //Escuchar el cambio de checkbox y le cambia el data-language
 flagsElement.addEventListener('change',(e)=> {
     if (e.target.checked) {
         flagsElement.setAttribute('data-language','esp')
         /* console.log(e.target.dataset.language); */
-        changeLanguages(e.target.dataset.language)
+        changeLanguages(e.target.dataset.language);
+        inputName.placeholder = 'Tu nombre';
+        inputEmail.placeholder = 'Tu correo electrónico';
+        inputMessage.placeholder = 'La descripción de tu proyecto';
+
     } else {
         flagsElement.setAttribute('data-language','eng')
         /* console.log(e.target.dataset.language); */
-        changeLanguages(e.target.dataset.language)
+        changeLanguages(e.target.dataset.language);
+        inputName.placeholder = 'Your name';
+        inputEmail.placeholder = 'Your email';
+        inputMessage.placeholder = 'Your project description';
     }
 });
 
